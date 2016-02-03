5c8e6c8645f1a39667a8d16489a9ee1546c9e4ea
indream <flashindream@gmail.com>
2016-02-03 12:31:35 +0800

    update modules

--- src/makeblock/makeblock.cxx
+++ src/makeblock/makeblock.cxx
@@ -24,6 +24,500 @@ MePort::MePort(){
 uint8_t MePort::getPin(uint8_t port,uint8_t slot){
 	return ports[port*2+slot];
 }
+/********************
+*******Me DC Motor
+*********************/
+MeDCMotor::MeDCMotor(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_pwm_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+	mraa_pwm_period_us(pin2,200);
+	mraa_pwm_enable(pin2,1);
+}
+MeDCMotor::~MeDCMotor (){
+    mraa_gpio_close (pin1);
+    mraa_pwm_close (pin2);
+}
+void MeDCMotor::run(int16_t pwm){
+	if(pwm>255){
+		pwm = 255;
+	}
+	if(pwm<-255){
+		pwm = -255;
+	}
+	uint8_t dir = 0;
+	if(pwm<0){
+		pwm = -pwm;
+		dir = 1;
+	}
+	mraa_pwm_write(pin2,pwm);
+	mraa_gpio_write(pin1,dir);
+}
+/********************
+*******Me Servo Motor
+*********************/
+MeServoMotor::MeServoMotor(uint8_t port,uint8_t slot){
+	MePort pt = MePort();
+	uint8_t s = pt.getPin(port,slot);
+	servo = Servo(s);
+	
+}
+MeServoMotor::~MeServoMotor (){
+	servo.haltPwm();
+}
+void MeServoMotor::run(uint8_t angle){
+	servo.setAngle(angle);
+}
+	
+/********************
+*******Me Stepper Motor
+*********************/
+
+typedef enum
+{
+  DIRECTION_CCW = 0,  ///< Clockwise
+  DIRECTION_CW  = 1   ///< Counter-Clockwise
+} Direction;
+
+MeStepperMotor::MeStepperMotor(uint8_t port){
+    mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_gpio_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+	mraa_gpio_dir(pin2, MRAA_GPIO_OUT);
+}
+~MeStepperMotor::MeStepperMotor (){
+    mraa_gpio_close (pin1);
+    mraa_gpio_close (pin2);
+}
+void MeStepperMotor::step(uint8_t dir){
+	mraa_gpio_write(pin1, dir==DIRECTION_CW?LOW:HIGH);
+	mraa_gpio_write(pin2,HIGH);
+	mraa_gpio_write(pin2,LOW);
+}
+bool MeStepperMotor::run(){
+	if((_speed == 0.0) || (distanceToGo() == 0))
+	{
+		return false;
+	}
+
+	if (runSpeed())
+	{
+		computeNewSpeed();
+		return true;
+	}
+}
+bool MeStepperMotor::runSpeed()
+{
+	// Dont do anything unless we actually have a step interval
+	if (!_stepInterval)
+	{
+		return false;
+	}
+
+	if (micros() - _lastStepTime > _stepInterval)
+	{
+		if (_dir == DIRECTION_CW)
+		{
+		  // Clockwise
+			_currentPos += 1;
+		}
+		else
+		{
+		  // Anticlockwise  
+			_currentPos -= 1;
+		}
+		step();
+		_lastStepTime = micros();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+long MeStepperMotor::distanceToGo()
+{
+	return _targetPos - _currentPos;
+}
+
+void MeStepperMotor::computeNewSpeed()
+{
+	long distanceTo = distanceToGo();
+	long stepsToStop = (long)((_speed * _speed) / (2.0 * _acceleration));
+	if (distanceTo == 0 && stepsToStop <= 1)
+	{
+		// We are at the target and its time to stop
+		_stepInterval = 0;
+		_speed = 0.0;
+		_n = 0;
+		return;
+	}
+
+	if (distanceTo > 0)
+	{
+		// We are anticlockwise from the target
+		// Need to go clockwise from here, maybe decelerate now
+		if (_n > 0)
+		{
+			// Currently accelerating, need to decel now? Or maybe going the wrong way?
+			if ((stepsToStop >= distanceTo) || _dir == DIRECTION_CCW)
+			{
+				_n = -stepsToStop; // Start deceleration
+			}
+		}
+		else if (_n < 0)
+		{
+			// Currently decelerating, need to accel again?
+			if ((stepsToStop < distanceTo) && _dir == DIRECTION_CW)
+			{
+				_n = -_n; // Start accceleration
+			}
+		}
+	}
+	else if (distanceTo < 0)
+	{
+		// We are clockwise from the target
+		// Need to go anticlockwise from here, maybe decelerate
+		if (_n > 0)
+		{
+			// Currently accelerating, need to decel now? Or maybe going the wrong way?
+			if ((stepsToStop >= -distanceTo) || _dir == DIRECTION_CW)
+			{
+				_n = -stepsToStop; // Start deceleration
+			}
+		}
+		else if (_n < 0)
+		{
+			// Currently decelerating, need to accel again?
+			if ((stepsToStop < -distanceTo) && _dir == DIRECTION_CCW)
+			{
+				_n = -_n; // Start accceleration
+			}
+		}
+	}
+
+	// Need to accelerate or decelerate
+	if (_n == 0)
+	{
+		// First step from stopped
+		_cn = _c0;
+		_dir = (distanceTo > 0) ? DIRECTION_CW : DIRECTION_CCW;
+	}
+	else
+	{
+		// Subsequent step. Works for accel (n is +_ve) and decel (n is -ve).
+		_cn = _cn - ((2.0 * _cn) / ((4.0 * _n) + 1)); // Equation 13
+		_cn = max(_cn, _cmin);
+	}
+	_n++;
+	_stepInterval = _cn;
+	_speed = 1000000.0 / _cn;
+	if (_dir == DIRECTION_CCW)
+	{
+		_speed = -_speed;
+	}
+}
+void MeStepperMotor::setMaxSpeed(float speed)
+{
+  if (_maxSpeed != speed)
+  {
+    _maxSpeed = speed;
+    _cmin = 1000000.0 / speed;
+    // Recompute _n from current speed and adjust speed if accelerating or cruising
+    if (_n > 0)
+    {
+      _n = (long)((_speed * _speed) / (2.0 * _acceleration)); // Equation 16
+      computeNewSpeed();
+    }
+  }
+}
+void MeStepperMotor::setSpeed(uint16_t speed){
+	if (speed == _speed){
+		return;
+	}
+	speed = constrain(speed, -_maxSpeed, _maxSpeed);
+	if (speed == 0.0){
+		_stepInterval = 0;
+	}else{
+		_stepInterval = fabs(1000000.0 /speed);
+		_dir = (speed > 0.0) ? DIRECTION_CW : DIRECTION_CCW;
+	}
+	_speed = speed;
+}
+void MeStepperMotor::setAcceleration(float acceleration){
+	if(acceleration == 0.0)
+	{
+		return;
+	}
+	if(_acceleration != acceleration)
+	{
+		_n = _n * (_acceleration / acceleration);
+		//	_c0 = sqrt(2.0 / acceleration) * 1000000.0;
+		// Accelerates at half the expected rate. Why?
+		_c0 = sqrt(1.0/acceleration) * 1000000.0;
+		_acceleration = acceleration;
+		computeNewSpeed();
+	}
+}
+void MeStepperMotor::move(long distance){
+	 moveTo(_currentPos + relative);
+}
+void MeStepperMotor::moveTo(long distance){
+	if (_targetPos != absolute)
+	{
+		_targetPos = absolute;
+		computeNewSpeed();
+	}
+}
+/********************
+*******Me7SegmentDisplay
+*********************/
+const uint8_t tubeTable[] = 
+{
+  0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, //0-9
+  0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71,                         //'A', 'B', 'C', 'D', 'E', 'F',
+  0xbf, 0x86, 0xdb, 0xcf, 0xe6, 0xed, 0xfd, 0x87, 0xff, 0xef, //0.-9.
+  0xf7, 0xfc, 0xb9, 0xde, 0xf9, 0xf1,                         //'A.', 'B.', 'C.', 'D.', 'E.', 'F.',
+  0, 0x40                                                     //' ','-'
+};
+Me7SegmentDisplay::Me7SegmentDisplay(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_gpio_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+	mraa_gpio_dir(pin2, MRAA_GPIO_OUT);
+	
+	set(BRIGHTNESS_2, ADDR_AUTO, STARTADDR);
+	clear();
+}
+~Me7SegmentDisplay::Me7SegmentDisplay (){
+    mraa_gpio_close (pin1);
+    mraa_gpio_close (pin2);
+}
+void Me7SegmentDisplay::set(uint8_t brightness, uint8_t SetData, uint8_t SetAddr)
+{
+  Cmd_SetData = SetData;
+  Cmd_SetAddr = SetAddr;
+  Cmd_DispCtrl = SEGDIS_ON + brightness;//Set brightness, take effect next display cycle.
+}
+void Me7SegmentDisplay::writeByte(uint8_t wr_data)
+{
+	uint8_t i;
+	uint8_t cnt0;
+	for (i = 0; i < 8; i++)  //sent 8bit data
+	{
+		mraa_gpio_write (pin2,LOW);
+		if (wr_data & 0x01)
+		{
+			mraa_gpio_write (pin1,HIGH); //LSB first
+		}
+		else
+		{
+			mraa_gpio_write (pin1,LOW);
+		}
+		wr_data >>= 1;
+		mraa_gpio_write (pin2,HIGH);
+	}
+	mraa_gpio_write (pin2,LOW); //wait for ACK
+	mraa_gpio_write (pin1,HIGH);
+	mraa_gpio_write (pin2,HIGH);
+	mraa_gpio_dir(pin1, MRAA_GPIO_IN);
+	while (mraa_gpio_read(pin1))
+	{
+		cnt0 += 1;
+		if (cnt0 == 200)
+		{
+			mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+			mraa_gpio_write(pin1, LOW);
+			cnt0 = 0;
+		}
+	}
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+}
+
+void Me7SegmentDisplay::start(void)
+{
+	mraa_gpio_write (pin2,HIGH);
+	mraa_gpio_write (pin1,HIGH);
+	mraa_gpio_write (pin1,LOW);
+	mraa_gpio_write (pin2,LOW);
+}
+void Me7SegmentDisplay::stop(void)
+{
+	mraa_gpio_write (pin2,LOW);
+	mraa_gpio_write (pin1,LOW);
+	mraa_gpio_write (pin2,HIGH);
+	mraa_gpio_write (pin1,HIGH);
+}
+void Me7SegmentDisplay::write(uint8_t SegData[])
+{
+	uint8_t i;
+	start();    // Start signal sent to TM1637 from MCU.
+	writeByte(ADDR_AUTO);
+	stop();
+	start();
+	writeByte(Cmd_SetAddr);
+	for (i = 0; i < 4; i++)
+	{
+		writeByte(SegData[i]);
+	}
+	stop();
+	start();
+	writeByte(Cmd_DispCtrl);
+	stop();
+}
+void Me7SegmentDisplay::display(float value){
+	uint8_t i=0;
+	bool isStart = false;
+	uint8_t index = 0;
+	uint8_t disp[]={0,0,0,0};
+	bool isNeg = false;
+	if((float)value<0)
+	{
+		isNeg = true;
+		value = -value;
+		disp[0] = 0x21;
+		index++;
+	}
+	for(i=0;i<7;i++)
+	{
+		int n = checkNum(value,3-i);
+		if(n>=1||i==3)
+		{
+			isStart=true;
+		}
+		if(isStart)
+		{
+			if(i==3)
+			{
+				disp[index]=n+0x10;
+			}
+			else
+			{
+				disp[index]=n;
+			}
+			index++;
+		}
+		if(index>3)
+		{
+			break;
+		}
+	}
+	display(disp);
+}
+void Me7SegmentDisplay::clear(void)
+{
+	uint8_t buf[4] = { ' ', ' ', ' ', ' ' };
+	display(buf);
+}
+int16_t Me7SegmentDisplay::checkNum(float v,int16_t b)
+{
+	if(b>=0)
+	{
+		return floor((v-floor(v/pow(10,b+1))*(pow(10,b+1)))/pow(10,b));
+	}
+	else
+	{
+		b=-b;
+		int i=0;
+		for(i=0;i<b;i++)
+		{
+			v = v*10;
+		}
+		return ((int)(v)%10);
+	}
+}
+
+/********************
+*******MeShutter
+*********************/	
+MeShutter::MeShutter(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_gpio_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+	mraa_gpio_dir(pin2, MRAA_GPIO_OUT);
+	mraa_gpio_write(pin1, LOW);
+	mraa_gpio_write(pin2, LOW);
+}
+MeShutter::~MeShutter (){
+	mraa_gpio_close(pin1);
+	mraa_gpio_close(pin2);
+}
+void MeShutter::shotOnTime(long time){
+	mraa_gpio_write(pin1,HIGH);
+	sleep(time);
+	mraa_gpio_write(pin1,LOW);
+}
+void MeShutter::shotOn(){
+	mraa_gpio_write(pin1,HIGH);
+}
+void MeShutter::shotOff(){
+	mraa_gpio_write(pin1,LOW);
+}
+void MeShutter::focusOn(){
+	mraa_gpio_write(pin2,HIGH);
+}
+void MeShutter::focusOff(){
+	mraa_gpio_write(pin2,LOW);
+}
+/********************
+*******MeUltrasonicSensor
+*********************/
 MeUltrasonicSensor::MeUltrasonicSensor(uint8_t port) {
     mraa_init();
 	MePort pt = MePort();
@@ -125,6 +619,93 @@ uint8_t MeLineFollower::read(){
 	return (mraa_gpio_read(pin1)<<1)+mraa_gpio_read(pin2);
 }
 /***********************
+*******Limit Switch
+************************/
+MeLimitSwitch::MeLimitSwitch(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_gpio_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_mode(pin1,MRAA_GPIO_PULLUP);
+	mraa_gpio_mode(pin2,MRAA_GPIO_PULLUP);
+	mraa_gpio_dir(pin1, MRAA_GPIO_IN);
+	mraa_gpio_dir(pin2, MRAA_GPIO_IN);
+}
+MeLimitSwitch::~MeLimitSwitch (){
+    mraa_gpio_close (pin1);
+    mraa_gpio_close (pin2);
+}
+uint8_t MeLimitSwitch::read(uint8_t slot){
+	return mraa_gpio_read(slot==0?pin1:pin2);
+}
+/***********************
+*******MeTouchSensor
+************************/
+MeTouchSensor::MeTouchSensor(uint8_t port){
+	
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_gpio_init(s1);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	pin2 = mraa_gpio_init(s2);
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin1, 1);
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin1, MRAA_GPIO_OUT);
+	mraa_gpio_dir(pin2, MRAA_GPIO_IN);
+}
+MeTouchSensor::~MeTouchSensor (){
+    mraa_gpio_close (pin1);
+    mraa_gpio_close (pin2);
+}
+uint8_t MeTouchSensor::read(){
+	return mraa_gpio_read(pin2);
+}
+uint8_t MeTouchSensor::setMode(uint8_t mode){
+	return mraa_gpio_write(pin1,mode);
+}
+/***********************
+*******MePIRMotionSensor
+************************/
+MePIRMotionSensor::MePIRMotionSensor(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s = pt.getPin(port,1);
+	pin = mraa_gpio_init(s);
+    if (pin == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin, 1);
+	mraa_gpio_dir(pin, MRAA_GPIO_IN);
+}
+MePIRMotionSensor::~MePIRMotionSensor (){
+	mraa_gpio_close(pin);
+}
+uint8_t MePIRMotionSensor::read(){
+	return mraa_gpio_read(pin);
+}
+/***********************
 *******Light Sensor
 ************************/
 MeLightSensor::MeLightSensor(uint8_t port) {
@@ -143,6 +724,134 @@ MeLightSensor::~MeLightSensor () {
 }
 uint16_t MeLightSensor::read(){
 	return mraa_aio_read(pin);
+}
+/***********************
+*******MeSoundSensor
+************************/
+MeSoundSensor::MeSoundSensor(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s2 = pt.getPin(port,1);
+	pin = mraa_aio_init(s2-14);
+    if (pin == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+}
+MeSoundSensor~MeSoundSensor(){
+	mraa_aio_close(pin);
+}
+uint16_t MeSoundSensorread(){
+	return mraa_aio_read(pin);
+}
+/***********************
+*******MePotentiometer
+************************/
+MePotentiometer::MePotentiometer(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s2 = pt.getPin(port,1);
+	pin = mraa_aio_init(s2-14);
+    if (pin == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+}
+MePotentiometer::~MePotentiometer(){
+	mraa_aio_close(pin);
+}
+uint16_t MePotentiometer::read(){
+	return mraa_aio_read(pin);
+}
+/***********************
+*******MeJoystick
+************************/
+MeJoystick::MeJoystick(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_aio_init(s1-14);
+	pin2 = mraa_aio_init(s2-14);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+}
+MeJoystick::~MeJoystick(){
+	mraa_aio_close(pin1);
+	mraa_aio_close(pin2);
+}
+uint16_t MeJoystick::read(uint8_t axis){
+	return mraa_aio_read(axis==0?pin1:pin2);
+}
+/***********************
+*******MeHumiture
+************************/
+MeFlameSensor::MeFlameSensor(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_aio_init(s1-14);
+	pin2 = mraa_gpio_init(s2);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin, MRAA_GPIO_IN);
+}
+MeFlameSensor::~MeFlameSensor(){
+	mraa_aio_close(pin1);
+	mraa_gpio_close(pin2);
+}
+uint16_t MeFlameSensor::read(uint8_t mode){
+	if(mode==0){
+		return mraa_aio_read(pin1);
+	}else{
+		return mraa_gpio_read(pin2);
+	}
+}
+/***********************
+*******MeGasSensor
+************************/
+MeGasSensor::MeGasSensor(uint8_t port){
+	mraa_init();
+	MePort pt = MePort();
+	uint8_t s1 = pt.getPin(port,0);
+	uint8_t s2 = pt.getPin(port,1);
+	pin1 = mraa_aio_init(s1-14);
+	pin2 = mraa_gpio_init(s2);
+    if (pin1 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s1);
+        exit (1);
+    }
+    if (pin2 == NULL) {
+        fprintf (stderr, "Are you sure that pin%d you requested is valid on your platform?", s2);
+        exit (1);
+    }
+	mraa_gpio_use_mmaped(pin2, 1);
+	mraa_gpio_dir(pin, MRAA_GPIO_IN);
+}
+MeGasSensor::~MeGasSensor(){
+	mraa_aio_close(pin1);
+	mraa_gpio_close(pin2);
+}
+uint16_t MeGasSensor::read(uint8_t mode){
+	if(mode==0){
+		return mraa_aio_read(pin1);
+	}else{
+		return mraa_gpio_read(pin2);
+	}
 }
 /***********************
 *******MeHumiture
--- src/makeblock/makeblock.h
+++ src/makeblock/makeblock.h
@@ -5,7 +5,7 @@
 #include <mraa/aio.h>
 #include <mraa/gpio.h>
 #include <sys/time.h>
-
+#include "servo.h"
 #define HIGH                   1
 #define LOW                    0
 #define PORT_1 1
@@ -35,11 +35,11 @@ namespace upm {
         public:
             MeDCMotor(uint8_t port);
 			~MeDCMotor ();
-            void run(uint8_t pwm);
+            void run(int16_t pwm);
 			
         private:
 			mraa_gpio_context pin1;
-			mraa_gpio_context pin2;
+			mraa_pwm_context  pin2;
     };
 	
 	class MeServoMotor {
@@ -49,7 +49,7 @@ namespace upm {
             void run(uint8_t angle);
 			
         private:
-			mraa_gpio_context pin;
+			Servo servo;
     };
 	
 	class MeStepperMotor {
@@ -57,38 +57,75 @@ namespace upm {
             MeStepperMotor(uint8_t port);
 			~MeStepperMotor ();
             void step(uint8_t dir);
-			void run();
+			bool run();
+			bool runSpeed();
 			void setSpeed(uint16_t speed);
-			void setAcc(uint16_t speed);
+			void setMaxSpeed(float speed);
+			void setAcceleration(float acceleration);
 			void move(long distance);
 			void moveTo(long distance);
 			
         private:
 			mraa_gpio_context pin1;
 			mraa_gpio_context pin2;
+			static uint8_t _dir_data;
+			static uint8_t _step_data;
+			uint8_t _dir;          
+			long _currentPos;     // Steps
+			long _targetPos;      // Steps
+			float _speed;         // Steps per second
+			float _maxSpeed;
+			float _acceleration;
+			unsigned long _stepInterval;
+			unsigned long _lastStepTime;
+			/// The step counter for speed calculations
+			long _n;
+			/// Initial step size in microseconds
+			float _c0;
+			/// Last step size in microseconds
+			float _cn;
+			/// Min step size in microseconds based on maxSpeed
+			float _cmin; // at max speed
     };
 	class Me7SegmentDisplay{
 		public:
 			Me7SegmentDisplay(uint8_t port);
 			~Me7SegmentDisplay ();
 			void display(float value);
+			void clear();
         private:
 			mraa_gpio_context pin1;
 			mraa_gpio_context pin2;
-	}
-	class Me7SegmentDisplay{
-		public:
-			Me7SegmentDisplay(uint8_t port);
-			~Me7SegmentDisplay ();
-			void display(float value);
-        private:
-			mraa_gpio_context pin1;
-			mraa_gpio_context pin2;
-	}
+			void set(uint8_t brightness, uint8_t SetData, uint8_t SetAddr);
+			void writeByte(uint8_t wr_data);
+			void start(void);
+			void stop(void);
+			void write(uint8_t SegData[]);
+			int16_t checkNum(float v,int16_t b);
+			/******************definitions for TM1637**********************/
+			const uint8_t   ADDR_AUTO = 0x40;   //Automatic address increment mode
+			const uint8_t   ADDR_FIXED = 0x44;   //Fixed address mode
+			const uint8_t   STARTADDR = 0xc0;   //start address of display register
+			const uint8_t   SEGDIS_ON = 0x88;   //diplay on
+			const uint8_t   SEGDIS_OFF = 0x80;   //diplay off
+			/**** definitions for the clock point of the digit tube *******/
+			const uint8_t POINT_ON = 1;
+			const uint8_t POINT_OFF = 0;
+			/**************definitions for brightness***********************/
+			const uint8_t BRIGHTNESS_0 = 0;
+			const uint8_t BRIGHTNESS_1 = 1;
+			const uint8_t BRIGHTNESS_2 = 2;
+			const uint8_t BRIGHTNESS_3 = 3;
+			const uint8_t BRIGHTNESS_4 = 4;
+			const uint8_t BRIGHTNESS_5 = 5;
+			const uint8_t BRIGHTNESS_6 = 6;
+			const uint8_t BRIGHTNESS_7 = 7;
+	}		
 	class MeShutter{
 		public:
 			MeShutter(uint8_t port);
 			~MeShutter ();
+			void shotOnTime(long time);
 			void shotOn();
 			void shotOff();
 			void focusOn();
@@ -143,7 +180,7 @@ namespace upm {
             MeTouchSensor(uint8_t port);
 			~MeTouchSensor ();
             uint8_t read();
-			
+			void setMode(uint8_t mode);
         private:
 			mraa_gpio_context pin1;
 			mraa_gpio_context pin2;
@@ -198,7 +235,7 @@ namespace upm {
         public:
             MeFlameSensor(uint8_t port);
 			~MeFlameSensor();
-            uint16_t read(uint8_t threshold);
+            uint16_t read(uint8_t mode=0);
 			
         private:
 			mraa_aio_context pin1;
@@ -208,7 +245,7 @@ namespace upm {
         public:
             MeGasSensor(uint8_t port);
 			~MeGasSensor();
-            uint16_t read(uint8_t threshold);
+            uint16_t read(uint8_t mode=0);
 			
         private:
 			mraa_aio_context pin1;
